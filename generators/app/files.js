269b31de14ae51444232ded5096a288d165e079d
Roberto Achar <robertoachar@gmail.com>
2020-01-07 14:55:13 -0300

    feature(template): add jest and jest-runner-eslint

--- generators/app/files.js
+++ generators/app/files.js
@@ -1,16 +1,25 @@
 const files = [
   { template: 'src/index.js.template', destination: 'src/index.js' },
+  { template: 'babel.config.js.template', destination: 'babel.config.js' },
   { template: 'CHANGELOG.md.template', destination: 'CHANGELOG.md' },
   { template: 'editorconfig.template', destination: '.editorconfig' },
   { template: 'eslintignore.template', destination: '.eslintignore' },
   { template: 'eslintrc.json.template', destination: '.eslintrc.json' },
   { template: 'gitattributes.template', destination: '.gitattributes' },
   { template: 'gitignore.template', destination: '.gitignore' },
+  {
+    template: 'jest-eslint.config.js.template',
+    destination: 'jest-eslint.config.js'
+  },
+  {
+    template: 'jest-test.config.js.template',
+    destination: 'jest-test.config.js'
+  },
+  { template: 'jest.config.js.template', destination: 'jest.config.js' },
   { template: 'LICENSE.template', destination: 'LICENSE' },
   { template: 'npmrc.template', destination: '.npmrc' },
   { template: 'main.js.template', destination: 'main.js' },
   { template: 'package.json.template', destination: 'package.json' },
-  { template: 'prettierrc.template', destination: '.prettierrc' },
   { template: 'README.md.template', destination: 'README.md' }
 ];
 
---
+++ generators/app/templates/jest-test.config.js.template
@@ -0,0 +1,7 @@
+module.exports = {
+  displayName: 'test',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/__tests__/**/*.js'],
+  collectCoverageFrom: ['**/src/**/**.js'],
+  verbose: true
+};

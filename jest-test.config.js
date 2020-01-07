ec12ac899db3adf004587ddab4cbe4a5afd56928
Roberto Achar <robertoachar@gmail.com>
2020-01-07 15:15:02 -0300

    test: remove main.js from test coverage

--- jest-test.config.js
+++ jest-test.config.js
@@ -2,6 +2,6 @@ module.exports = {
   displayName: 'test',
   testEnvironment: 'node',
   testMatch: ['<rootDir>/__tests__/**/*.js'],
-  collectCoverageFrom: ['**/generators/**/**.js'],
+  collectCoverageFrom: ['**/generators/**/**.js', '!**/generators/**/main.js'],
   verbose: true
 };
